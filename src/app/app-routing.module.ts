7089f989683864408c71ed33e5007df77232347e
Ioana Chiriacescu <i.chiriacescu@stabiplan.ro>
2022-10-25 23:46:53 +0300

    route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { FirstComponent } from './first/first.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SecondComponent } from './second/second.component';
+import { FirstActivateGuard } from './services/first-activate.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'first', component: FirstComponent },
+  { path: 'first', component: FirstComponent, canActivate: [FirstActivateGuard] },
   { path: 'second/:id', component: SecondComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }, //wildcard
